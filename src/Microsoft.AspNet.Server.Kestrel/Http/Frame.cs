3c8faf17a2e3cb3df55d9b76dcdda9b9f2678e90
Ben Adams <thundercat@illyriad.co.uk>
2015-11-21 14:57:32 +0000

    st changes

--- src/Microsoft.AspNet.Server.Kestrel/Http/Frame.cs
+++ src/Microsoft.AspNet.Server.Kestrel/Http/Frame.cs
@@ -54,6 +54,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
         private volatile bool _requestProcessingStopping; // volatile, see: https://msdn.microsoft.com/en-us/library/x13ttww7.aspx
         private volatile bool _requestAborted;
         private CancellationTokenSource _abortedCts;
+        private CancellationToken? _manuallySetRequestAbortToken;
 
         private FrameRequestStream _requestBody;
         private FrameResponseStream _responseBody;
@@ -136,7 +137,47 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
         public Stream DuplexStream { get; set; }
 
-        public CancellationToken RequestAborted { get; set; }
+        public CancellationToken RequestAborted
+        {
+            get
+            {
+                // If a request abort token was previously explicitly set, return it.
+                if (_manuallySetRequestAbortToken.HasValue)
+                    return _manuallySetRequestAbortToken.Value;
+
+                // Otherwise, get the abort CTS.  If we have one, which would mean that someone previously
+                // asked for the RequestAborted token, simply return its token.  If we don't,
+                // check to see whether we've already aborted, in which case just return an
+                // already canceled token.  Finally, force a source into existence if we still
+                // don't have one, and return its token.
+                var cts = _abortedCts;
+                return
+                    cts != null ? cts.Token :
+                    _requestAborted ? new CancellationToken(true) :
+                    RequestAbortedSource.Token;
+            }
+            set
+            {
+                // Set an abort token, overriding one we create internally.  This setter and associated
+                // field exist purely to support IHttpRequestLifetimeFeature.set_RequestAborted.
+                _manuallySetRequestAbortToken = value;
+            }
+        }
+
+        private CancellationTokenSource RequestAbortedSource
+        {
+            get
+            {
+                // Get the abort token, lazily-initializing it if necessary.
+                // Make sure it's canceled if an abort request already came in.
+                var cts = LazyInitializer.EnsureInitialized(ref _abortedCts, () => new CancellationTokenSource());
+                if (_requestAborted)
+                {
+                    cts.Cancel();
+                }
+                return cts;
+            }
+        }
 
         public bool HasResponseStarted
         {
@@ -189,7 +230,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
 
             _prepareRequest?.Invoke(this);
 
-            _abortedCts?.Dispose();
+            _manuallySetRequestAbortToken = null;
             _abortedCts = null;
         }
 
@@ -247,15 +288,7 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                 ConnectionControl.End(ProduceEndType.SocketDisconnect);
                 SocketInput.AbortAwaiting();
 
-                try
-                {
-                    _abortedCts?.Cancel();
-                }
-                catch (ObjectDisposedException)
-                {
-                    // Don't log ODEs thrown from _abortedCts.Cancel()
-                    // If _abortedCts is disposed, the app has already completed.
-                }
+                RequestAbortedSource.Cancel();
             }
             catch (Exception ex)
             {
@@ -263,7 +296,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             }
             finally
             {
-                _abortedCts?.Dispose();
                 _abortedCts = null;
             }
         }
@@ -311,8 +343,8 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
                         ResponseBody = _responseBody;
                         DuplexStream = new FrameDuplexStream(RequestBody, ResponseBody);
 
-                        _abortedCts = new CancellationTokenSource();
-                        RequestAborted = _abortedCts.Token;
+                        _abortedCts = null;
+                        _manuallySetRequestAbortToken = null;
 
                         var httpContext = HttpContextFactory.Create(this);
                         try
@@ -365,7 +397,6 @@ namespace Microsoft.AspNet.Server.Kestrel.Http
             {
                 try
                 {
-                    _abortedCts?.Dispose();
                     _abortedCts = null;
 
                     // If _requestAborted is set, the connection has already been closed.
